845a9c912a12ff909b5f2d820427f06facaf8bf4
guilherme.silva <guilherme.silva@rentbrella.com>
2024-10-22 00:01:41 -0300

    reset de senha

--- organizai-api/src/controllers/UserController.ts
+++ organizai-api/src/controllers/UserController.ts
@@ -9,8 +9,6 @@ import bcrypt from 'bcryptjs';
 export class UserController {
   private userRepository = AppDataSource.getRepository(User);
   private catRepository = AppDataSource.getRepository(Categoria);
-  
-
 
   createUser = async (req: Request, res: Response) => {
     try {
@@ -20,14 +18,14 @@ export class UserController {
       const newUser = new User();
       Object.assign(newUser, userDto); // copia o dto para a nova instancia
 
-       // Validar a entidade
-       const errors = await validate(newUser);
-       if (errors.length > 0) {
-         const errorMessages = errors.map(error => 
-           Object.values(error.constraints || {})
-         ).flat();
-         return res.status(400).json({ errors: errorMessages });
-       }
+      // Validar a entidade
+      const errors = await validate(newUser);
+      if (errors.length > 0) {
+        const errorMessages = errors
+          .map((error) => Object.values(error.constraints || {}))
+          .flat();
+        return res.status(400).json({ errors: errorMessages });
+      }
       // Buscar todas as categorias
       const categorias = await this.catRepository.find();
 
@@ -36,7 +34,7 @@ export class UserController {
 
       const saltRounds = 10; // Define o número de rounds de salt
       newUser.senha = await bcrypt.hash(newUser.senha, saltRounds);
-      
+
       // Salvar o usuário com as categorias associadas
       const savedUser = await this.userRepository.save(newUser);
 
@@ -49,10 +47,15 @@ export class UserController {
       res.status(201).json(savedUser);
     } catch (error) {
       console.error(error);
-      if (error instanceof QueryFailedError && error.message.includes('duplicate key value violates unique constraint')) {
-        return res.status(409).json({ message: "Este email já está cadastrado" });
+      if (
+        error instanceof QueryFailedError &&
+        error.message.includes('duplicate key value violates unique constraint')
+      ) {
+        return res
+          .status(409)
+          .json({ message: 'Este email já está cadastrado' });
       }
-      res.status(500).json({ message: "Erro ao criar usuário" });
+      res.status(500).json({ message: 'Erro ao criar usuário' });
     }
   };
 
@@ -60,38 +63,38 @@ export class UserController {
     try {
       const users = await this.userRepository.find();
       if (!users || users.length === 0) {
-         res.status(404).json({ message: "Usuários não encontrados" });
+        res.status(404).json({ message: 'Usuários não encontrados' });
       }
       res.json(users);
     } catch (error) {
       console.error(error);
-       res.status(500).json({ message: "Erro ao buscar usuários" });
+      res.status(500).json({ message: 'Erro ao buscar usuários' });
     }
   };
 
   findUserByEmail = async (req: Request, res: Response) => {
     try {
-        const { email } = req.params; // Captura o email do path (req.params)
-    
-        if (!email) {
-           return res.status(400).json({ message: "Email é obrigatório" });
-        }
-    
-        const user = await this.userRepository.findOneBy({ email });
-    
-        if (!user) {
-           return res.status(404).json({ message: "Usuário não encontrado" });
-        } else {
-          const userWithCategoriesAndQuiz = await this.userRepository.findOne({
-            where: { UserId: user.UserId },
-            relations: ['categorias', 'quiz']
-          });
-          return res.json(userWithCategoriesAndQuiz);
-        }
-      } catch (error) {
-        console.error(error);
-         return res.status(500).json({ message: "Erro ao buscar usuário" });
+      const { email } = req.params; // Captura o email do path (req.params)
+
+      if (!email) {
+        return res.status(400).json({ message: 'Email é obrigatório' });
       }
+
+      const user = await this.userRepository.findOneBy({ email });
+
+      if (!user) {
+        return res.status(404).json({ message: 'Usuário não encontrado' });
+      } else {
+        const userWithCategoriesAndQuiz = await this.userRepository.findOne({
+          where: { UserId: user.UserId },
+          relations: ['categorias', 'quiz'],
+        });
+        return res.json(userWithCategoriesAndQuiz);
+      }
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Erro ao buscar usuário' });
+    }
   };
 
   autenticacaoUser = async (req: Request, res: Response) => {
@@ -99,28 +102,57 @@ export class UserController {
       const { email, senha } = req.body;
 
       if (!email || !senha) {
-        return res.status(400).json({ message: "Email e senha são obrigatórios" });
+        return res
+          .status(400)
+          .json({ message: 'Email e senha são obrigatórios' });
       }
 
       // Busca o usuário pelo email
       const user = await this.userRepository.findOneBy({ email });
 
       if (!user) {
-        return res.status(404).json({ message: "Usuário não encontrado" });
+        return res.status(404).json({ message: 'Usuário não encontrado' });
       }
 
       // Verifica se a senha está correta
       const senhaValida = await bcrypt.compare(senha, user.senha);
 
       if (!senhaValida) {
-        return res.status(401).json({ message: "Senha incorreta" });
+        return res.status(401).json({ message: 'Senha incorreta' });
       }
 
       // Caso a autenticação seja bem-sucedida
       return res.sendStatus(200); // Sucesso, sem retorno de dados
     } catch (error) {
       console.error(error);
-      return res.status(500).json({ message: "Erro ao autenticar usuário" });
+      return res.status(500).json({ message: 'Erro ao autenticar usuário' });
+    }
+  };
+  resetPassword = async (req: Request, res: Response) => {
+    try {
+      const { email, newPassword } = req.body;
+
+      if (!email || !newPassword) {
+        return res
+          .status(400)
+          .json({ message: 'Email e nova senha são obrigatórios' });
+      }
+
+      const user = await this.userRepository.findOneBy({ email });
+
+      if (!user) {
+        return res.status(404).json({ message: 'Usuário não encontrado' });
+      }
+
+      const saltRounds = 10;
+      user.senha = await bcrypt.hash(newPassword, saltRounds);
+
+      await this.userRepository.save(user);
+
+      return res.status(200).json({ message: 'Senha atualizada com sucesso' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Erro ao redefinir senha' });
     }
   };
 }
